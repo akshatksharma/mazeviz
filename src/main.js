057a95a9a8c09385f0cb3834a2652455f99f1682
akshatksharma <akshatksharma@wustl.edu>
2020-06-01 02:20:29 -0500

    Removed outline for button selection for desktop users

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import grid from "./grid.js";
 
+function handleFirstTab(e) {
+  if (e.keyCode === 9) {
+    // the "I am a keyboard user" key
+    document.body.classList.add("user-is-tabbing");
+    window.removeEventListener("keydown", handleFirstTab);
+  }
+}
+
+window.addEventListener("keydown", handleFirstTab);
+
 let wallList = [];
 let startLoc = [14, 10];
 let endLoc = [14, 30];
