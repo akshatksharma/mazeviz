d12221caba5898e4471d5c1835b618bda39bed3b
Akshat Sharma <akshatsharma@MacBook-Pro.local>
2020-07-21 01:30:01 -0500

    Added removeable close dialog

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import {main} from "./main.js";
+import { main } from "./main.js";
+
+const warning = document.getElementsByClassName("info--text--warning")[0]
+  .parentElement;
+
+warning.onclick = () => {
+  warning.style.opacity = 0;
+};
 
 main();
